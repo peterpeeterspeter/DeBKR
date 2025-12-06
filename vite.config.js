3f06758b74dd3cdbd9d9439782d2405ceadeb23f
peterpeeterspeter <101089521+peterpeeterspeter@users.noreply.github.com>
2025-12-06 22:23:46 +0100

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  optimizeDeps: {
+    include: ['@supabase/supabase-js']
+  },
   server: {
     port: 3000,
     proxy: {
